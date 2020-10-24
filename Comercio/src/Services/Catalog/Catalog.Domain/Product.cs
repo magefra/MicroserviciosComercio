2d7c639eea87ba1853806397134fd6b588964870
= <=>
2020-10-24 11:31:25 -0500

    Agregando el modelo de catalog

---
+++ Comercio/src/Services/Catalog/Catalog.Domain/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Domain
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+
+        public decimal Price { get; set; }
+
+        public ProductStock Stock { get; set; }
+    }
+}
